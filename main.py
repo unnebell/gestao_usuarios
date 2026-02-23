cb829a88144ff4159def28491c93c9e59fe0c739
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-02-23 15:38:09 -0300

    feat: implementação do url_for
    Benefícios do URL_FOR
    Fácil manutenção
    Evita erros de digitação
    Gera URLs dinâmicas
    Mais segurança

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 #inicialização
 app = Flask(__name__)
@@ -6,7 +6,14 @@ app = Flask(__name__)
 #rotas
 @app.route('/')
 def ola_mundo():
-    return "Olá, mundo!"
+    return f"<a href='{url_for('pagina_sobre')}'>Página sobre</a>"
+
+@app.route('/sobre')
+def pagina_sobre():
+    return """
+        <b>Programador Python</b>
+        <p>Testando criação de rotas</p>
+    """
 
 #execução
 app.run(debug=True)
