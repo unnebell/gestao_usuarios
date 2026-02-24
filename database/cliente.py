94851e017f3b5ff8de4dd013d125093b7c67ed0d
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-02-24 09:26:21 -0300

    update: modificação email

--- database/cliente.py
+++ database/cliente.py
@@ -1,9 +1,9 @@
 CLIENTES = [
-    {"id": 1, "nome": "Fábio", "email": "fabiokaua13hs@gmail.com"},
+    {"id": 1, "nome": "Fábio", "email": "testefabio@gmail.com"},
     {"id": 2, "nome": "Pedro", "email": "testepedro@gmail.com"},
-    {"id": 3, "nome": "Thiago", "email": "thiagonobarco@gmail.com"},
-    {"id": 4, "nome": "João", "email": "joaopedefeijao@gmail.com"},
-    {"id": 5, "nome": "Roberto", "email": "robertoapenas@gmail.com"},
-    {"id": 6, "nome": "Felipe", "email": "pelifeff@gmail.com"},
-    {"id": 7, "nome": "Guilherme", "email": "guiandlherme@gmail.com"},
+    {"id": 3, "nome": "Thiago", "email": "testethiago@gmail.com"},
+    {"id": 4, "nome": "João", "email": "testejoao@gmail.com"},
+    {"id": 5, "nome": "Roberto", "email": "testeroberto@gmail.com"},
+    {"id": 6, "nome": "Felipe", "email": "testefelipe@gmail.com"},
+    {"id": 7, "nome": "Guilherme", "email": "testeguilherme@gmail.com"},
 ]
