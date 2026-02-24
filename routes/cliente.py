10ed10189328e86405a96a3b72d372caec3ffe14
Fábio Silva <124391193+unnebell@users.noreply.github.com>
2026-02-24 10:45:25 -0300

    feature: criação do DELETE e início do PUT

--- routes/cliente.py
+++ routes/cliente.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from database.cliente import CLIENTES
 
 cliente_route = Blueprint('cliente', __name__)
@@ -9,9 +9,21 @@ def lista_clientes():
     return render_template('lista_clientes.html', clientes=CLIENTES)
 
 @cliente_route.route('/', methods=['POST'])
-def inserir_cliente(cliente_id):
+def inserir_cliente():
     #inserir os dados do cliente no bd
-    pass 
+
+    data = request.json          #pega os inputs do front-end
+
+    novo_usuario = {
+        "id": len(CLIENTES) + 1, #isso torna o id como auto incremento 
+        "nome": data['nome'],    #recebe o nome que está locado na variável data
+        "email": data['email'],  #recebe o email que está locado na variável data
+    }
+
+    CLIENTES.append(novo_usuario)
+
+    return render_template('item_cliente.html', cliente=novo_usuario)
+
 
 @cliente_route.route('/new')
 def form_cliente():
@@ -25,7 +37,13 @@ def detalhe_cliente(cliente_id):
 @cliente_route.route('/<int:cliente_id>/edit')
 def form_edit_cliente(cliente_id):
     #formulario para editar um cliente
-    return render_template("form_edit_cliente.html")
+
+    cliente = None
+    for c in CLIENTES:
+        if c['id'] == cliente_id:
+            cliente = c
+
+    return render_template("form_cliente.html", cliente=cliente)
 
 @cliente_route.route('/<int:cliente_id>/update', methods=['PUT'])
 def atualizar_cliente(cliente_id):
@@ -35,4 +53,7 @@ def atualizar_cliente(cliente_id):
 @cliente_route.route('/<int:cliente_id>/delete', methods=['DELETE'])
 def deletar_cliente(cliente_id):
     #apaga o registro de um cliente
-    pass 
+    global CLIENTES
+    CLIENTES = [ c for c in CLIENTES if c ['id'] != cliente_id] 
+
+    return {'deleted': 'ok'}
